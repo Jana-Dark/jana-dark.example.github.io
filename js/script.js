498cb7cbe9564cc88699a949d8fe532556556b71
Jana-Dark <pannoptikum666@gmail.com>
2022-04-20 13:35:42 +0300

    1
    2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 
-/*  let user;
- let userName ="vasya";
- user=userName;
- console.log(user);
 
  const mainTitle = document.querySelector ("#main-title");
  const mainHeaderMenu =document.querySelectorAll("#main-header-menu");
@@ -25,7 +21,7 @@ const aboutItemImage =
  document.querySelector(".about-item-image");
 
 
- window.addEventListener( "scroll", function (){
+ /*window.addEventListener( "scroll", function (){
      let scrollAboutImage =window.scrollY;
 
      if (scrollAboutImage>700) {
@@ -38,7 +34,106 @@ const aboutItemImage =
      console.log(scrollAboutImage);
  }); */
 
-$(document).ready(function() {
+ console.log('Init!');
+
+ // inputmask
+ const form = document.querySelector('form');
+ const telSelector = form.querySelector('input[type="tel"]');
+ const inputMask = new Inputmask ('+38 (099)-999-99-99');
+ inputMask.mask(telSelector);
+
+ const validation = new JustValidate('.form');
+ validation
+     .addField('.input-name', [
+      {
+       rule: 'minLength',
+       value: 3,
+      },
+      {
+       rule: 'maxLength',
+       value: 15,
+      },
+      {
+       rule: 'required',
+       value: true,
+       errorMessage: 'Введите имя!',
+      }
+     ])
+     .addField('.input-tel', [
+      {
+       rule: 'required',
+       value: true,
+       errorMessage: 'Введите телефон',
+      },
+      {
+       rule: 'function',
+       validator: function() {
+        const phone = telSelector.inputmask.unmaskedvalue();
+        return phone.length === 10;
+       },
+       errorMessage: 'Введите корректный телефон',
+      },
+     ]).onSuccess((event) => {
+     console.log('Validation passes and form submitted', event);
+
+     let formData = new FormData(event.target);
+
+     console.log(...formData);
+
+     let xhr = new XMLHttpRequest();
+
+     xhr.onreadystatechange = function () {
+         if (xhr.readyState === 4) {
+             if (xhr.status === 200) {
+                 console.log('Отправлено');
+             }
+         }
+     }
+
+     xhr.open('POST', 'mail.php', true);
+     xhr.send(formData);
+
+     event.target.reset();
+ });
+
+
+
+
+ /*new window.JustValidate('.form', {
+  rules: {
+   name: {
+    required: true
+   },
+
+   tel : {
+     required: true,
+     function: () => {
+     const phone =telSelector.inputmask.unmaskedvalue();
+     return Number (phone) && phone.length === 10;
+    }
+   }
+  },
+  messages :{
+   name: {
+    required : 'Введите имя',
+    minLength: 'Введите 3 и более символов',
+    maxLength: 'Запрещено воодить более 15 символов'
+   }
+  },
+  submitHandler: function(thisForm) {
+
+  }
+
+ })*/
+
+
+
+
+
+
+
+
+/*$(document).ready(function() {
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
@@ -57,4 +152,4 @@ $(document).ready(function() {
         return false;
     });
 
-});
+});*/
